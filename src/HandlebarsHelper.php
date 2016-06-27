b8b013355e1717ee11b91e08fa9fd4770a38f869
Brendan Quigley <brendan@beequeue.co.uk>
2016-06-28 00:12:20 +0100

    Relax argument type for addHelper until we establish interface to use

--- src/HandlebarsHelper.php
+++ src/HandlebarsHelper.php
@@ -13,10 +13,20 @@ class HandlebarsHelper
     private $helperMethods = [];
     private $helpers = [];
 
-    public function addHelper($id, HelperInterface $helper)
+    public function addHelper($id, $helper)
     {
-        $this->helperMethods[$id] = get_class($helper) . '::handle';
-        $this->helpers[$id] = [$helper, 'handle'];
+        // This is a temporary measure until we decide on a common interface.
+        // I'm relaxing the argument type above, but doing the check here to
+        // determine the helper's invocation method
+
+        if (is_a($helper, 'JaySDe\HandlebarsBundle\Helper\HelperInterface')) {
+            $method = 'handle';
+        } else {
+            $method = 'execute';
+        }
+
+        $this->helperMethods[$id] = get_class($helper) . '::' . $method;
+        $this->helpers[$id] = [$helper, $method];
     }
 
     public function getHelperMethods()
