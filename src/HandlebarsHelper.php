fcb06b30244e221060ca3df7e26d79deacd4271d
Jens Schulze <jens.schulze@commercetools.de>
2016-07-05 18:13:26 +0200

    refactor(Helper): use new helper resolver for external helpers

--- src/HandlebarsHelper.php
+++ src/HandlebarsHelper.php
@@ -10,18 +10,16 @@ use JaySDe\HandlebarsBundle\Helper\HelperInterface;
 
 class HandlebarsHelper
 {
-    private $helperMethods = [];
     private $helpers = [];
 
     public function addHelper($id, HelperInterface $helper)
     {
-        $this->helperMethods[$id] = get_class($helper) . '::handle';
         $this->helpers[$id] = [$helper, 'handle'];
     }
 
     public function getHelperMethods()
     {
-        return $this->helperMethods;
+        return array_keys($this->helpers);
     }
 
     public function getHelpers()
