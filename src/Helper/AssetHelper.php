d570947902aa901ef3535ffc7c38d48945a18a65
Jens Schulze <jens.schulze@commercetools.de>
2016-09-22 14:11:19 +0200

    feat(Helper): add asset and routing (url, path) helper by default

---
+++ src/Helper/AssetHelper.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @author @jayS-de <jens.schulze@commercetools.de>
+ */
+
+namespace JaySDe\HandlebarsBundle\Helper;
+
+use Symfony\Component\Asset\Packages;
+
+class AssetHelper implements HelperInterface
+{
+    private $packages;
+
+    public function __construct(Packages $packages)
+    {
+        $this->packages = $packages;
+    }
+    
+    public function handle($context, $options)
+    {
+        $packageName = isset($options['hash']['package']) ? $options['hash']['package'] : null;
+
+        return $this->packages->getUrl($context, $packageName);
+    }
+}
---
+++ src/Helper/RoutingHelper.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @author @jayS-de <jens.schulze@commercetools.de>
+ */
+
+namespace JaySDe\HandlebarsBundle\Helper;
+
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class RoutingHelper implements HelperInterface
+{
+    private $generator;
+    private $type;
+
+    public function __construct(UrlGeneratorInterface $generator, $type)
+    {
+        $this->generator = $generator;
+        $this->type = $type;
+    }
+    
+    public function handle($context, $options)
+    {
+        $options = isset($options['hash']) ? $options['hash'] : [];
+        $method = 'get' . ucfirst($this->type);
+        return $this->$method($context, $options);
+    }
+
+    private function getPath($name, $parameters = array(), $relative = false)
+    {
+        return $this->generator->generate($name, $parameters, $relative ? UrlGeneratorInterface::RELATIVE_PATH : UrlGeneratorInterface::ABSOLUTE_PATH);
+    }
+
+    private function getUrl($name, $parameters = array(), $schemeRelative = false)
+    {
+        return $this->generator->generate($name, $parameters, $schemeRelative ? UrlGeneratorInterface::NETWORK_PATH : UrlGeneratorInterface::ABSOLUTE_URL);
+    }
+}
