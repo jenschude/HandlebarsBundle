2fba9764826ed0a4333794e2a1d33a2bd652989d
Jens Schulze <jens.schulze@commercetools.de>
2017-02-28 12:06:28 +0100

    add functional test

---
+++ src/Tests/FunctionalTest.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * @author @jayS-de <jens.schulze@commercetools.de>
+ */
+
+namespace JaySDe\HandlebarsBundle\Tests;
+
+use JaySDe\HandlebarsBundle\Cache\Filesystem;
+use JaySDe\HandlebarsBundle\HandlebarsEngine;
+use JaySDe\HandlebarsBundle\HandlebarsEnvironment;
+use JaySDe\HandlebarsBundle\HandlebarsHelperService;
+use JaySDe\HandlebarsBundle\HandlebarsProfileExtension;
+use JaySDe\HandlebarsBundle\Loader\FilesystemLoader;
+use JaySDe\HandlebarsBundle\Tests\Fixtures\TestBundle\TestBundle;
+use LightnCandy\LightnCandy;
+use Symfony\Bundle\FrameworkBundle\Templating\Loader\TemplateLocator;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\Templating\TemplateNameParser;
+
+class FunctionalTest extends \PHPUnit_Framework_TestCase
+{
+    public function testRender()
+    {
+        $locator = new FileLocator();
+        $templateLocator = new TemplateLocator($locator);
+        $parser = new TemplateNameParser();
+        $loader = new FilesystemLoader($templateLocator, $parser);
+        $loader->addPath(__DIR__ . '/Fixtures/Resources/views');
+        $this->addPaths($loader);
+
+        $helperService = new HandlebarsHelperService();
+
+        $cacheDir = __DIR__ . '/Fixtures/cache';
+        $cache = new Filesystem(__DIR__ . '/Fixtures/cache');
+
+        $profile = new \Twig_Profiler_Profile();
+        $profiler = new HandlebarsProfileExtension($profile);
+        $environment = new HandlebarsEnvironment(
+            $loader,
+            $helperService,
+            ['flags' => LightnCandy::FLAG_BESTPERFORMANCE | LightnCandy::FLAG_ERROR_EXCEPTION],
+            $cache,
+            $profiler
+        );
+        $engine = new HandlebarsEngine($environment, $parser);
+
+        $result = $engine->render('main.hbs');
+        $this->assertSame('Hello world!', trim($result));
+
+        array_map('unlink', glob("$cacheDir/**/*.*"));
+        array_map('rmdir', glob("$cacheDir/**"));
+        $this->assertTrue(rmdir($cacheDir));
+    }
+
+    private function addPaths(FilesystemLoader $loader)
+    {
+        // register bundles as Handlebars namespaces
+        foreach (['TestBundle' => TestBundle::class] as $bundle => $class) {
+            $dir = __DIR__.'/Fixtures/Resources/'.$bundle.'/views';
+            if (is_dir($dir)) {
+                $loader->addPath(
+                    $dir,
+                    $this->normalizeBundleNameForLoaderNamespace($bundle)
+                );
+            }
+            $reflection = new \ReflectionClass($class);
+            $dir = dirname($reflection->getFileName()).'/Resources/views';
+            if (is_dir($dir)) {
+                $loader->addPath(
+                    $dir,
+                    $this->normalizeBundleNameForLoaderNamespace($bundle)
+                );
+            }
+        }
+    }
+
+    private function normalizeBundleNameForLoaderNamespace($bundle)
+    {
+        if ('Bundle' === substr($bundle, -6)) {
+            $bundle = substr($bundle, 0, -6);
+        }
+
+        return $bundle;
+    }
+}
